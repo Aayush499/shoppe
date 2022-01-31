14c8f0380b0ef2bd16bec1833b963d709e1bc2a9
Aayush499 <partypooper24>
2022-01-31 21:19:47 +0530

    Init

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,167 @@
+import React, { useState } from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd';
+import FileUpload from '../../utils/FileUpload'
+import Axios from 'axios';
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const Continents = [
+    { key: 1, value: "Hot" },
+    { key: 2, value: "Cold" },
+    { key: 3, value: "Medium" },
+    { key: 4, value: "Sweet" },
+    { key: 5, value: "Spicy" },
+    { key: 6, value: "Fresh" },
+    { key: 7, value: "Frozen" }
+]
+
+const Meat = [
+  { key: 1, value: "Veg" },
+  { key: 2, value: "Non-Veg" }
+]
+
+const Rating = [
+  { key: 1, value: "1" },
+  { key: 2, value: "2" },
+  { key: 3, value: "3" },
+  { key: 4, value: "4" },
+  { key: 5, value: "5" },
+  { key: 6, value: "0" }
+]
+function UploadProductPage(props) {
+
+    const [TitleValue, setTitleValue] = useState("")
+    const [DescriptionValue, setDescriptionValue] = useState("")
+    const [PriceValue, setPriceValue] = useState(0)
+    const [ContinentValue, setContinentValue] = useState(1)
+    const [MeatValue, setMeatValue] = useState(1)
+    const [RatingValue, setRatingValue] = useState(6)
+    const [Images, setImages] = useState([])
+
+
+    const onTitleChange = (event) => {
+        setTitleValue(event.currentTarget.value)
+    }
+
+    const onDescriptionChange = (event) => {
+        setDescriptionValue(event.currentTarget.value)
+    }
+
+    const onPriceChange = (event) => {
+        setPriceValue(event.currentTarget.value)
+    }
+
+    const onContinentsSelectChange = (event) => {
+        setContinentValue(event.currentTarget.value)
+    }
+    const onMeatSelectChange = (event) => {
+      setMeatValue(event.currentTarget.value)
+  }
+  const onRatingSelectChange = (event) => {
+    setRatingValue(event.currentTarget.value)
+}
+    const updateImages = (newImages) => {
+        setImages(newImages)
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+
+        if (!TitleValue || !DescriptionValue || !PriceValue ||
+            !ContinentValue || !Images) {
+            return alert('fill all the fields first!')
+        }
+
+        const variables = {
+            writer: props.user.userData._id,
+            title: TitleValue,
+            description: DescriptionValue,
+            price: PriceValue,
+            images: Images,
+            continents: ContinentValue,
+            meat: MeatValue,
+            rating: RatingValue,
+        }
+
+        Axios.post('/api/product/uploadProduct', variables)
+            .then(response => {
+                if (response.data.success) {
+                    alert('Product Successfully Uploaded')
+                    props.history.push('/')
+                } else {
+                    alert('Failed to upload Product')
+                }
+            })
+
+    }
+
+    return (
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <Title level={2}> Upload food</Title>
+            </div>
+
+
+            <Form onSubmit={onSubmit} >
+
+                {/* DropZone */}
+                <FileUpload refreshFunction={updateImages} />
+
+                <br />
+                <br />
+                <label>Title</label>
+                <Input
+                    onChange={onTitleChange}
+                    value={TitleValue}
+                />
+                <br />
+                <br />
+                <label>Description</label>
+                <TextArea
+                    onChange={onDescriptionChange}
+                    value={DescriptionValue}
+                />
+                <br />
+                <br />
+                <label>Price($)</label>
+                <Input
+                    onChange={onPriceChange}
+                    value={PriceValue}
+                    type="number"
+                />
+                <br /><br />
+                <select onChange={onContinentsSelectChange} value={ContinentValue}>
+                    {Continents.map(item => (
+                        <option key={item.key} value={item.key}>{item.value} </option>
+                    ))}
+                </select>
+                <br />
+                <br />
+                <select onChange={onMeatSelectChange} value={MeatValue}>
+                    {Meat.map(item => (
+                        <option key={item.key} value={item.key}>{item.value} </option>
+                    ))}
+                </select>
+                <br />
+                <br />
+                <select onChange={onRatingSelectChange} value={RatingValue}>
+                    {Rating.map(item => (
+                        <option key={item.key} value={item.key}>{item.value} </option>
+                    ))}
+                </select>
+                <br />
+                <br />
+                <Button
+                    onClick={onSubmit}
+                >
+                    Submit
+                </Button>
+
+            </Form>
+
+        </div>
+    )
+}
+
+export default UploadProductPage
